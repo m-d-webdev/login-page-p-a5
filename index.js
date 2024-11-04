ea76c2411695b530e3e6e36c2744f0311b400d06
MUS-js-1 <mstph.iderkaoui@gmail.com>
2024-11-04 10:45:11 +0100

    Ajoutons le project

---
+++ index.js
@@ -0,0 +1,6 @@
+function ba9za1() {
+    document.getElementById("container").style.backgroundImage="url(ba9zayad.jpg)";
+}
+function ba9za2() {
+    document.getElementById("container").style.background="black";
+}
